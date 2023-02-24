32a17db72c4587f286c2243f24f7ba3dbeaaea1c
Brune <abrune.osteo@gmail.com>
2023-02-24 21:29:23 +0100

    font family change and adding a state to listen to textarea

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,25 @@
+import React, {useState} from 'react';
+
 export default function App() {
+  const [text, setText] = useState("");
+  console.log(text)
+    function handleChange(event) {
+      
+      const {value} = event.target
+      setText(value)
+    }
+
+
   return(
-    <h1>Hello</h1>
+    <div>
+    <h1>How fast do you type?</h1>
+    <textarea
+    value={text}
+    onChange={handleChange}
+    />
+    <h4>Time remaining : ???</h4>
+    <button>Start</button>
+    <h1>Word count: ???</h1>
+    </div>
   )
 }
