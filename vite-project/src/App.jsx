1db7a4b5ae52ddc2c5ad1064638a261acf9225d4
Brune <abrune.osteo@gmail.com>
2023-02-27 21:58:02 +0100

    added function to count words written in text area

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,19 @@ import React, {useState} from 'react';
 export default function App() {
   const [text, setText] = useState("");
   console.log(text)
+
     function handleChange(event) {
-      
       const {value} = event.target
       setText(value)
     }
+
+    function handleCount(text) {
+      const wordsArr = text.trim().split(" ")
+      const filteredWords = wordsArr.filter(word => word !== "")
+      return filteredWords.length
+    }
+
+    console.log(handleCount(text))
 
 
   return(
@@ -18,7 +26,7 @@ export default function App() {
     onChange={handleChange}
     />
     <h4>Time remaining : ???</h4>
-    <button>Start</button>
+    <button onClick={() => console.log(handleCount(text))}>Start</button>
     <h1>Word count: ???</h1>
     </div>
   )
